fb0c26d96ce3f99a6a4e1c10cda39037d03e5d6e
neil <naha.hrithkk@gmail.com>
2024-04-24 13:17:58 +0530

    Update 0682-baseball-game.py

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -3,27 +3,14 @@ class Solution:
 
         result = []
 
-        while operations:
-            operation = operations.pop(0)
-
+        for operation in operations:
             if operation not in "+DC":
                 result.append(int(operation))
             elif operation == "C":
                 result.pop()
             elif operation == "D":
-                val = result.pop()
-                result.append(val)
-                result.append(2*val)
+                result.append(result[-1] * 2)
             elif operation == "+":
-                val1 = result.pop()
-                val2 = result.pop()
-                result.append(val2)
-                result.append(val1)
-                result.append(val1 + val2)
+                result.append(result[-1] + result[-2])
         
-        sum = 0
-        
-        for num in result:
-            sum += num
-        
-        return sum
+        return sum(result)
